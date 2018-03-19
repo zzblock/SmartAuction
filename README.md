4665e27376cc9b00af22af0ea3b333bc374fe6ad
kumar <zoeb.sheikh74@gmail.com>
2018-03-19 14:51:05 +0530

    init smart auction

---
+++ README.md
@@ -0,0 +1,6 @@
+## Running
+
+step1. Clone this repo
+step2. `npm install`
+step3. Make sure `testrpc` is running on its default port. Then:
+  - `npm run start` - Starts the development server 3001
---
+++ truffle.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rpc: {
+    host: '127.0.0.1',
+    port: 8545,
+    gas: 190000
+  },
+  // networks: {
+  //   development: {
+  //     host: "localhost",
+  //     port: 8545,
+  //     network_id: "*" // Match any network id
+  //   }
+  // },
+  migrations_directory: './migrations'
+}
